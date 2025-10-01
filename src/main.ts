8e9244e71533563d188a0cae587317875c3d3fc8
AnthoniBortolotto <antho@outlook.com.br>
2025-09-30 21:24:10 -0300

    Package - class validator added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   // add 30 seconds timeout
   const server = app.getHttpServer();
   server.setTimeout(30000);
